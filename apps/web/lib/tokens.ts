347cfaf8253dd659589612530e993eefa04a63dd
kadirchan <kadircanbozkurt1905@gmail.com>
2024-08-24 22:08:50 +0300

    list of tokens  helper

---
+++ apps/web/lib/tokens.ts
@@ -0,0 +1,24 @@
+"use client";
+
+export const tokens = [
+  {
+    name: "MINA",
+    icon: "/icons/mina.svg",
+    tokenId: "0",
+  },
+  {
+    name: "USDT",
+    icon: "/icons/usdt.svg",
+    tokenId: "1",
+  },
+  {
+    name: "ETH",
+    icon: "/icons/eth.svg",
+    tokenId: "2",
+  },
+  {
+    name: "BTC",
+    icon: "/icons/btc.svg",
+    tokenId: "3",
+  },
+];
