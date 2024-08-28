017bb908a15f17418a10f7a696f5d36c8746642a
kadirchan <kadircanbozkurt1905@gmail.com>
2024-08-29 01:38:12 +0300

    critical fix AAAAAAAHHHHHHHH

--- apps/web/components/chain.tsx
+++ apps/web/components/chain.tsx
@@ -65,7 +65,7 @@ export function Chain({ height }: ChainProps) {
           if (!pool) {
             return;
           }
-          // console.log(pool);
+          console.log(pool);
           const token0Id = pool.tokenA.toString();
           const token1Id = pool.tokenB.toString();
 
@@ -77,7 +77,11 @@ export function Chain({ height }: ChainProps) {
               TokenId.from(poolId.toString()),
             );
 
-          if (token0Id < token1Id) {
+          console.log(BigInt(token0Id));
+          console.log(BigInt(token1Id));
+          console.log(BigInt(token0Id) < BigInt(token1Id));
+
+          if (BigInt(token0Id) < BigInt(token1Id)) {
             const pool: Pool = {
               poolId: poolId.toString(),
               token0: tokenList.find((token) => token.tokenId === token0Id)!,
@@ -87,19 +91,33 @@ export function Chain({ height }: ChainProps) {
               lpTokenSupply: lpTokenSupply?.toString() ?? "0",
             };
 
-            // console.log(pool);
+            console.log("31", pool);
             poolList.push(pool);
           } else {
+            const smallerTokenId = token1Id;
+            console.log("smallerTokenId", smallerTokenId);
+            const smallerToken = tokenList.find(
+              (token) => token.tokenId === token1Id,
+            );
+            console.log("smallerToken", smallerToken);
+            console.log("smallerTokenAmount", token1Amount);
+            const biggerTokenId = token0Id;
+            console.log("biggerTokenId", biggerTokenId);
+            const biggerToken = tokenList.find(
+              (token) => token.tokenId === token0Id,
+            );
+            console.log("biggerToken", biggerToken);
+            console.log("biggerTokenAmount", token0Amount);
             const pool: Pool = {
               poolId: poolId.toString(),
               token0: tokenList.find((token) => token.tokenId === token1Id)!,
               token1: tokenList.find((token) => token.tokenId === token0Id)!,
-              token1Amount,
-              token0Amount,
+              token0Amount: token1Amount,
+              token1Amount: token0Amount,
               lpTokenSupply: lpTokenSupply?.toString() ?? "0",
             };
 
-            // console.log(pool);
+            console.log("52", pool);
             poolList.push(pool);
           }
 
